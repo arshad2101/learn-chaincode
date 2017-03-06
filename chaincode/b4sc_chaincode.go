0be15cc7c7111387422c5e8af995e84bd7fd1db4
arshad2101 <arshad_1983@rediffmail.com>
2017-03-06 15:17:52 +0530

    ss

--- chaincode/b4sc_chaincode.go
+++ chaincode/b4sc_chaincode.go
@@ -14,87 +14,158 @@ type B4SCChaincode struct {
 //custom data models
 
 type Pallet struct {
-	PalletId		string
-	Modeltype		string
-	CartonId		[]string
-	WayBill			[]string	
+	PalletId  string
+	Modeltype string
+	CartonId  []string
+	WayBill   []string
 }
 
 type Carton struct {
-	CartonId		string
-	AssetId			[]string
+	CartonId string
+	AssetId  []string
 }
 
 type Asset struct {
-	AssetId			string
-	Modeltype		string
-	color			string
+	AssetId   string
+	Modeltype string
+	color     string
 }
 
+type WayBill struct {
+	WayBillId        string
+	Consigner        string
+	ConsignerAddress string
+	Consignee        string
+	ConsigneeAddress string
+	ConsigneeRegNo   string
+	LastModifiedDate string
+	Quantity         int
+	Assets           []Asset
+	Cartons          []Carton
+	Pallets          []Pallet
+}
 
+type CreateWayBillRequest struct {
+	WayBillId        string
+	Consigner        string
+	ConsignerAddress string
+	Consignee        string
+	ConsigneeAddress string
+	ConsigneeRegNo   string
+	LastModifiedDate string
+	Quantity         int
+	Assets           []Asset
+	Cartons          []Carton
+	Pallets          []Pallet
+}
+
+type CreateWayBillResponse struct {
+	Err     string `json:"err"`
+	ErrMsg  string `json:"errMsg"`
+	Message string `json:"message"`
+}
+
+type MWayBill struct {
+	MWayBillId       string
+	CreatedDate      string
+	LastModifiedDate string
+	Status           string
+	ConsignerAddress string
+	Consignee        string
+	ConsigneeAddress string
+	ConsigneeRegNo   string
+	ModelNo          string
+	VehicleNumber    string
+	VehicleType      string
+	PickUpTime       string
+	ValuesOfGood     string
+	ConsignerNotes   string
+	CreatedBy        string
+	PendingWith      string
+	Pallets          []Pallet
+	Cartons          []Carton
+	Assets           []Asset
+}
+type MMWayBill struct {
+	MMWayBillId          string
+	CreatedDate          string
+	LastModifiedDate     string
+	Status               string
+	ConatinerNo          string
+	ConsignerAddress     string
+	Consignee            string
+	ConsigneeAddress     string
+	ConsigneeRegNo       string
+	PersonConsigning     string
+	VehicleId            string
+	ExportWarehouseNotes string
+	CreatedBy            string
+	PendingWith          string
+	Conatiner            string
+	MWayBills            []MWayBill
+	Pallets              []Pallet
+	Cartons              []Carton
+	Assets               []Asset
+}
 type WayBillHistory struct {
-	name 	string
-	address string
-	timestamp  string
-	lat		string
-	log		string
+	name      string
+	address   string
+	timestamp string
+	lat       string
+	log       string
 }
 
 type Note struct {
-	Org		string
-	Notes	string
+	Org   string
+	Notes string
 }
-
 
 type Shipment struct {
-	ShipmentNumber		string
-	WayBillNo        	string
-	WayBillType        	string
-	PersonConsigningGoods	string
-	Consigner      		string
-	ConsignerAddress	string
-	Consignee      		string
-	ConsigneeAddress	string
-	ConsigneeRegNo		string
-	Quantity           	string
-	Pallets         	[]string
-	Cartons         	[]string
-	Status				string
-	ModelNo				string			
-	VehicleNumber		string
-	VehicleType			string
-	PickUpTime			string
-	ValueOfGoods		string
-	AllNotes			[]Note
-	ContainerId			string
-	WayBills 			[]string
-	WayBillHistorys		[]WayBillHistory
-	Carrier				string
-	Acl					[]string
-	CreatedBy        	string
-	Custodian			string
-	CreatedTimeStamp	string
-	UpdatedTimeStamp	string
+	ShipmentNumber        string
+	WayBillNo             string
+	WayBillType           string
+	PersonConsigningGoods string
+	Consigner             string
+	ConsignerAddress      string
+	Consignee             string
+	ConsigneeAddress      string
+	ConsigneeRegNo        string
+	Quantity              string
+	Pallets               []string
+	Cartons               []string
+	Status                string
+	ModelNo               string
+	VehicleNumber         string
+	VehicleType           string
+	PickUpTime            string
+	ValueOfGoods          string
+	AllNotes              []Note
+	ContainerId           string
+	WayBills              []string
+	WayBillHistorys       []WayBillHistory
+	Carrier               string
+	Acl                   []string
+	CreatedBy             string
+	Custodian             string
+	CreatedTimeStamp      string
+	UpdatedTimeStamp      string
 }
 
-
 type ShipmentIndex struct {
-	ShipmentNumber		string
-	Status				string
-	Acl					[]string
+	ShipmentNumber string
+	Status         string
+	Acl            []string
 }
 
 type AllShipment struct {
 	ShipmentIndexArr []ShipmentIndex
 }
 
-
-
 /************** Create Shipment Starts ************************/
 /**
-	Expected Input is 
+	Expected Input is
 	{
-		"shipmentNumber"" : "123456", 
+		"shipmentNumber"" : "123456",
 		"personConsigningGoods" : "KarthikS",
 		"consigner" : "HCL",
 		"consignerAddress" : "Chennai",
@@ -113,29 +184,46 @@ type AllShipment struct {
 **/
 
 type CreateShipmentRequest struct {
-	ShipmentNumber		string		`json:"shipmentNumber"`        
-	PersonConsigningGoods	string	`json:"personConsigningGoods"`
-	Consigner      		string   	`json:"consigner"`
-	ConsignerAddress	string   	`json:"consignerAddress"`
-	Consignee      		string   	`json:"consignee"`
-	ConsigneeAddress	string   	`json:"consigneeAddress"`
-	ConsigneeRegNo		string   	`json:"consigneeRegNo"`
-	ModelNo				string		`json:"modelNo"`	
-	Quantity           	string   	`json:"quantity"`
-	Pallets         	[]string 	`json:"pallets"`
-	Carrier				string		`json:"status"`
-	Notes				string		`json:"notes"`
-	CreatedBy        	string		`json:"createdBy"`
-	Custodian			string		`json:"custodian"`
-	CreatedTimeStamp	string		`json:"createdTimeStamp"`
-	CallingEntityName	string		`json:"callingEntityName"`
-	
+	ShipmentNumber        string   `json:"shipmentNumber"`
+	PersonConsigningGoods string   `json:"personConsigningGoods"`
+	Consigner             string   `json:"consigner"`
+	ConsignerAddress      string   `json:"consignerAddress"`
+	Consignee             string   `json:"consignee"`
+	ConsigneeAddress      string   `json:"consigneeAddress"`
+	ConsigneeRegNo        string   `json:"consigneeRegNo"`
+	ModelNo               string   `json:"modelNo"`
+	Quantity              string   `json:"quantity"`
+	Pallets               []string `json:"pallets"`
+	Carrier               string   `json:"status"`
+	Notes                 string   `json:"notes"`
+	CreatedBy             string   `json:"createdBy"`
+	Custodian             string   `json:"custodian"`
+	CreatedTimeStamp      string   `json:"createdTimeStamp"`
+	CallingEntityName     string   `json:"callingEntityName"`
 }
 
 type CreateShipmentResponse struct {
-	Err			string		`json:"err"`
-	ErrMsg		string		`json:"errMsg"`
-	Message		string		`json:"message"`
+	Err     string `json:"err"`
+	ErrMsg  string `json:"errMsg"`
+	Message string `json:"message"`
+}
+
+func CreateWayBill(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Entering Master Master WayBill")
+
+	wayBillRequest := parseWayBillRequest(args[0])
+
+	return processWayBill(stub, wayBillRequest)
+
+}
+
+func CreateMMWayBill(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	fmt.Println("Entering Master Master WayBill")
+
+	shipmentRequest := parseCreateShipmentRequest(args[0])
+
+	return processShipment(stub, shipmentRequest)
+
 }
 
 func CreateShipment(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
@@ -145,10 +233,9 @@ func CreateShipment(stub shim.ChaincodeStubInterface, args []string) ([]byte, er
 
 	return processShipment(stub, shipmentRequest)
 
-
 }
 
-func processShipment(stub shim.ChaincodeStubInterface, shipmentRequest CreateShipmentRequest) ([]byte, error){
+func processShipment(stub shim.ChaincodeStubInterface, shipmentRequest CreateShipmentRequest) ([]byte, error) {
 	shipment := Shipment{}
 	shipmentIndex := ShipmentIndex{}
 
@@ -168,7 +255,6 @@ func processShipment(stub shim.ChaincodeStubInterface, shipmentRequest CreateShi
 	shipment.CreatedTimeStamp = shipmentRequest.CreatedTimeStamp
 	shipment.Status = "Created"
 
-
 	manufacturerNotes := Note{}
 	manufacturerNotes.Org = shipmentRequest.CallingEntityName
 	manufacturerNotes.Notes = shipmentRequest.Notes
@@ -179,12 +265,11 @@ func processShipment(stub shim.ChaincodeStubInterface, shipmentRequest CreateShi
 	var acl []string
 	acl = append(acl, shipmentRequest.CallingEntityName)
 	shipment.Acl = acl
-	
 
 	shipmentIndex.ShipmentNumber = shipmentRequest.ShipmentNumber
 	shipmentIndex.Status = shipment.Status
 	shipmentIndex.Acl = acl
-	
+
 	dataToStore, _ := json.Marshal(shipment)
 
 	err := stub.PutState(shipment.ShipmentNumber, []byte(dataToStore))
@@ -193,7 +278,7 @@ func processShipment(stub shim.ChaincodeStubInterface, shipmentRequest CreateShi
 		return nil, err
 	}
 
-	addShipmentIndex(stub , shipmentIndex)
+	addShipmentIndex(stub, shipmentIndex)
 
 	resp := CreateShipmentResponse{}
 	resp.Err = "000"
@@ -204,24 +289,56 @@ func processShipment(stub shim.ChaincodeStubInterface, shipmentRequest CreateShi
 	fmt.Println("Successfully saved way bill")
 	return []byte(respString), nil
 
+}
+
+func processWayBill(stub shim.ChaincodeStubInterface, createWayBillRequest CreateWayBillRequest) ([]byte, error) {
+	wayBill := WayBill{}
+	//	shipmentIndex := ShipmentIndex{}
+
+	wayBill.WayBillId = createWayBillRequest.WayBillId
+	wayBill.Consigner = createWayBillRequest.Consigner
+	wayBill.ConsignerAddress = createWayBillRequest.ConsignerAddress
+	wayBill.Consignee = createWayBillRequest.Consignee
+	wayBill.ConsigneeAddress = createWayBillRequest.ConsigneeAddress
+	wayBill.LastModifiedDate = createWayBillRequest.LastModifiedDate
+	wayBill.Quantity = createWayBillRequest.Quantity
+	wayBill.Assets = createWayBillRequest.Assets
+	wayBill.Cartons = createWayBillRequest.Cartons
+	wayBill.Pallets = createWayBillRequest.Pallets
+
+	dataToStore, _ := json.Marshal(wayBill)
+
+	err := stub.PutState(wayBill.WayBillId, []byte(dataToStore))
+	if err != nil {
+		fmt.Println("Could not save WayBill to ledger", err)
+		return nil, err
+	}
+
+	resp := CreateWayBillResponse{}
+	resp.Err = "000"
+	resp.Message = wayBill.WayBillId
+
+	respString, _ := json.Marshal(resp)
+
+	fmt.Println("Successfully saved way bill")
+	return []byte(respString), nil
 
 }
 
-
-func addShipmentIndex(stub shim.ChaincodeStubInterface, shipmentIndex ShipmentIndex)(error){
+func addShipmentIndex(stub shim.ChaincodeStubInterface, shipmentIndex ShipmentIndex) error {
 	indexByte, err := stub.GetState("SHIPMENT_INDEX")
 	if err != nil {
 		fmt.Println("Could not retrive Shipment Index", err)
 		return err
 	}
 	allShipmentIndex := AllShipment{}
-	
-	if marshErr := json.Unmarshal(indexByte, &allShipmentIndex); marshErr != nil {
-        fmt.Println("Could not save Shipment to ledger", marshErr)
-		return marshErr
-    }
 
-	allShipmentIndex.ShipmentIndexArr = append(allShipmentIndex.ShipmentIndexArr, shipmentIndex);
+	if marshErr := json.Unmarshal(indexByte, &allShipmentIndex); marshErr != nil {
+		fmt.Println("Could not save Shipment to ledger", marshErr)
+		return marshErr
+	}
+
+	allShipmentIndex.ShipmentIndexArr = append(allShipmentIndex.ShipmentIndexArr, shipmentIndex)
 	dataToStore, _ := json.Marshal(allShipmentIndex)
 
 	addErr := stub.PutState("SHIPMENT_INDEX", []byte(dataToStore))
@@ -233,8 +350,20 @@ func addShipmentIndex(stub shim.ChaincodeStubInterface, shipmentIndex ShipmentIn
 	return nil
 }
 
-
 func parseCreateShipmentRequest(jsondata string) CreateShipmentRequest {
+	res := CreateShipmentRequest{}
+	json.Unmarshal([]byte(jsondata), &res)
+	fmt.Println(res)
+	return res
+}
+
+func parseWayBillRequest(jsondata string) CreateWayBillRequest {
+	res := CreateWayBillRequest{}
+	json.Unmarshal([]byte(jsondata), &res)
+	fmt.Println(res)
+	return res
+}
+func parseMMWayBillRequest(jsondata string) CreateShipmentRequest {
 	res := CreateShipmentRequest{}
 	json.Unmarshal([]byte(jsondata), &res)
 	fmt.Println(res)
@@ -246,31 +375,28 @@ func parseCreateShipmentRequest(jsondata string) CreateShipmentRequest {
 /************** View Shipment Starts ************************/
 
 type ViewShipmentRequest struct {
-	CallingEntityName		string		`json:"callingEntityName"`        
-	ShipmentNumber			string		`json:"shipmentNumber"`	
+	CallingEntityName string `json:"callingEntityName"`
+	ShipmentNumber    string `json:"shipmentNumber"`
 }
 
 func ViewShipment(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	fmt.Println("Entering ViewShipment " + args[0])
 
 	request := parseViewShipmentRequest(args[0])
-	
 
 	shipmentData, dataerr := fetchShipmentData(stub, request.ShipmentNumber)
 	if dataerr == nil {
 		if hasPermission(shipmentData.Acl, request.CallingEntityName) {
 			dataToStore, _ := json.Marshal(shipmentData)
-			return []byte(dataToStore),nil
+			return []byte(dataToStore), nil
 		} else {
-			return []byte("{ \"errMsg\": \"No data found\" }"),nil
+			return []byte("{ \"errMsg\": \"No data found\" }"), nil
 		}
-	} 
-	
-	return nil,dataerr
-	
-	
-}
+	}
 
+	return nil, dataerr
+
+}
 
 func parseViewShipmentRequest(jsondata string) ViewShipmentRequest {
 	res := ViewShipmentRequest{}
@@ -284,7 +410,7 @@ func parseViewShipmentRequest(jsondata string) ViewShipmentRequest {
 /************** Inbox Service Starts ************************/
 
 /**
-	Expected Input is 
+	Expected Input is
 	{
 		"callingEntityName" : "INTEL",
 		"status" : "Created"
@@ -292,23 +418,20 @@ func parseViewShipmentRequest(jsondata string) ViewShipmentRequest {
 **/
 
 type InboxRequest struct {
-	CallingEntityName		string		`json:"callingEntityName"`        
-	Status					string		`json:"status"`	
+	CallingEntityName string `json:"callingEntityName"`
+	Status            string `json:"status"`
 }
 
 type InboxResponse struct {
-	Data		[]Shipment		`json:"data"`
+	Data []Shipment `json:"data"`
 }
-
 
 func Inbox(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	fmt.Println("Entering Inbox " + args[0])
 
 	request := parseInboxRequest(args[0])
-	
-	
-	return fetchShipmentIndex(stub, request.CallingEntityName, request.Status)
 
+	return fetchShipmentIndex(stub, request.CallingEntityName, request.Status)
 
 }
 
@@ -319,10 +442,10 @@ func parseInboxRequest(jsondata string) InboxRequest {
 	return res
 }
 
-func hasPermission(acl []string, currUser string) (bool){
+func hasPermission(acl []string, currUser string) bool {
 	lenOfArray := len(acl)
 
-	for i :=0; i < lenOfArray; i++ {
+	for i := 0; i < lenOfArray; i++ {
 		if acl[i] == currUser {
 			return true
 		}
@@ -341,32 +464,31 @@ func fetchShipmentData(stub shim.ChaincodeStubInterface, shipmentNumber string) 
 	}
 
 	if marshErr := json.Unmarshal(indexByte, &shipmentData); marshErr != nil {
-        fmt.Println("Could not save Shipment to ledger", marshErr)
+		fmt.Println("Could not save Shipment to ledger", marshErr)
 		return shipmentData, marshErr
-    }
+	}
 
 	return shipmentData, nil
 
 }
 
-func fetchShipmentIndex(stub shim.ChaincodeStubInterface, callingEntityName string, status string)([]byte, error){
+func fetchShipmentIndex(stub shim.ChaincodeStubInterface, callingEntityName string, status string) ([]byte, error) {
 	allShipmentIndex := AllShipment{}
-	var shipmentIndexArr  []ShipmentIndex
+	var shipmentIndexArr []ShipmentIndex
 	var tmpShipmentIndex ShipmentIndex
 	var shipmentDataArr []Shipment
 	resp := InboxResponse{}
-
 
 	indexByte, err := stub.GetState("SHIPMENT_INDEX")
 	if err != nil {
 		fmt.Println("Could not retrive Shipment Index", err)
 		return nil, err
 	}
-		
+
 	if marshErr := json.Unmarshal(indexByte, &allShipmentIndex); marshErr != nil {
-        fmt.Println("Could not save Shipment to ledger", marshErr)
+		fmt.Println("Could not save Shipment to ledger", marshErr)
 		return nil, marshErr
-    }
+	}
 
 	shipmentIndexArr = allShipmentIndex.ShipmentIndexArr
 
@@ -387,33 +509,27 @@ func fetchShipmentIndex(stub shim.ChaincodeStubInterface, callingEntityName stri
 	resp.Data = shipmentDataArr
 	dataToStore, _ := json.Marshal(resp)
 
-	return []byte(dataToStore),nil
+	return []byte(dataToStore), nil
 }
 
 /************** Inbox Service Ends ************************/
 
-
 /************** View Data for Key Starts ************************/
-
-
 
 func ViewDataForKey(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 	fmt.Println("Entering ViewDataForKey " + args[0])
 
 	return stub.GetState(args[0])
 
-
 }
 
 /************** View Data for Key Ends ************************/
 
-
 /************** DumpData Start ************************/
 
 func DumpData(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	fmt.Println("Entering DumpData " + args[0] + "  "+ args[1])
+	fmt.Println("Entering DumpData " + args[0] + "  " + args[1])
 
-	
 	err := stub.PutState(args[0], []byte(args[1]))
 	if err != nil {
 		fmt.Println("Could not save the Data", err)
@@ -425,14 +541,12 @@ func DumpData(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
 
 /************** DumpData Ends ************************/
 
-
-
 // Init resets all the things
 func (t *B4SCChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	fmt.Println("Inside INIT for test chaincode")
 
 	allShipment := AllShipment{}
-	var tmpShipmentIndex  []ShipmentIndex
+	var tmpShipmentIndex []ShipmentIndex
 	allShipment.ShipmentIndexArr = tmpShipmentIndex
 
 	dataToStore, _ := json.Marshal(allShipment)
@@ -449,20 +563,22 @@ func (t *B4SCChaincode) Init(stub shim.ChaincodeStubInterface, function string, 
 func (t *B4SCChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	/*if function == "Init" {
 		return Init(stub, function, args)
-	}else*/ 
+	}else*/
 	if function == "CreateShipment" {
 		return CreateShipment(stub, args)
-	}  else {
+	} else if function == "CreateWayBill" {
+		return CreateWayBill(stub, args)
+	} else {
 		return nil, errors.New("Invalid function name " + function)
 	}
 	//return nil, nil
 }
 
 func (t *B4SCChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	
+
 	if function == "ViewShipment" {
 		return ViewShipment(stub, args)
-	}else if function == "ViewDataForKey" {
+	} else if function == "ViewDataForKey" {
 		return ViewDataForKey(stub, args)
 	} else if function == "Inbox" {
 		return Inbox(stub, args)
